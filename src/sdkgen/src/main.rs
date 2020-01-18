2cb4f6476f803bd866fde9de5bce43bb67bb76b1
Marshall Bowers <elliott.codes@gmail.com>
2020-01-17 21:44:40 -0500

    Convert apiDoc routes to sdkgen routes

--- src/sdkgen/src/main.rs
+++ src/sdkgen/src/main.rs
@@ -1,9 +1,35 @@
-use apidoc::Route;
+use apidoc;
+use sdkgen_core::{HttpMethod, Route};
+
+fn convert_http_method(method: apidoc::HttpMethod) -> HttpMethod {
+    match method {
+        apidoc::HttpMethod::Get => HttpMethod::Get,
+        apidoc::HttpMethod::Post => HttpMethod::Post,
+        apidoc::HttpMethod::Put => HttpMethod::Put,
+        apidoc::HttpMethod::Delete => HttpMethod::Delete,
+    }
+}
+
+fn convert_route(route: apidoc::Route) -> Route {
+    Route {
+        name: route.name,
+        method: convert_http_method(route.method),
+        url: route.url,
+        group: route.group,
+        version: route.version,
+        url_parameters: vec![],
+        payload_type: None,
+        return_type: None,
+    }
+}
 
 fn main() {
     let json = include_str!("../../../../../serve_apidoc/api_data.json");
 
-    let routes: Vec<Route> = serde_json::from_str(json).expect("Failed to deserialize API data");
+    let routes: Vec<apidoc::Route> =
+        serde_json::from_str(json).expect("Failed to deserialize API data");
+
+    let routes: Vec<Route> = routes.into_iter().map(convert_route).collect();
 
     println!("{:?}", routes);
 
--- src/sdkgen_core/src/lib.rs
+++ src/sdkgen_core/src/lib.rs
@@ -1,7 +1,78 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+#[derive(Debug)]
+pub enum Primitive {
+    String,
+    Boolean,
+    Integer,
+    Float,
+    Double,
+}
+
+#[derive(Debug)]
+pub enum Type {
+    Primitive(Primitive),
+    Array(Box<Type>),
+    Map { key: Box<Type>, value: Box<Type> },
+    Union { name: String, cases: Vec<String> },
+    Record { name: String, members: Vec<Member> },
+}
+
+impl Type {
+    pub fn set_name<N: Into<String>>(mut self, name: N) -> Self {
+        let new_name = name;
+
+        match self {
+            Type::Union { ref mut name, .. } | Type::Record { ref mut name, .. } => {
+                *name = new_name.into();
+            }
+            Type::Primitive(_) | Type::Array(_) | Type::Map { .. } => (),
+        };
+
+        self
     }
+}
+
+#[derive(Debug)]
+pub struct Member {
+    pub name: String,
+    pub description: Option<String>,
+    pub ty: Type,
+    pub is_optional: bool,
+}
+
+#[derive(Debug)]
+pub enum HttpMethod {
+    Get,
+    Post,
+    Put,
+    Delete,
+}
+
+#[derive(Debug)]
+pub struct Route {
+    pub name: String,
+    pub method: HttpMethod,
+    pub url: String,
+    pub group: String,
+    pub version: String,
+    pub url_parameters: Vec<UrlParameter>,
+    pub payload_type: Option<Type>,
+    pub return_type: Option<Type>,
+}
+
+#[derive(Debug)]
+pub struct UrlParameter {
+    pub name: String,
+    pub ty: Primitive,
+}
+
+#[derive(Debug)]
+pub struct SdkVersion {
+    pub version: String,
+    pub resources: Vec<SdkResource>,
+}
+
+#[derive(Debug)]
+pub struct SdkResource {
+    pub resource: String,
+    pub routes: Vec<Route>,
 }
