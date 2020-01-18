4e0aebcd7abd934ced8ab41d007e27058ab32d68
Marshall Bowers <elliott.codes@gmail.com>
2020-01-17 21:06:44 -0500

    Add deserialization for apiDoc data

--- src/sdkgen/src/main.rs
+++ src/sdkgen/src/main.rs
@@ -1,3 +1,11 @@
+use apidoc::Route;
+
 fn main() {
+    let json = include_str!("../../../../../serve_apidoc/api_data.json");
+
+    let routes: Vec<Route> = serde_json::from_str(json).expect("Failed to deserialize API data");
+
+    println!("{:?}", routes);
+
     println!("Hello, world!");
 }
