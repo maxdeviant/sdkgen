6258062fba704a0a3664561042140495e645390b
Marshall Bowers <elliott.codes@gmail.com>
2021-05-14 23:14:32 -0400

    Remove remnants of apiDoc

--- src/sdkgen/src/main.rs
+++ src/sdkgen/src/main.rs
@@ -1,61 +1,9 @@
 use std::collections::HashMap;
 
-use apidoc;
 use emitter_csharp::CsharpSdk;
 use emitter_typescript::TypeScriptSdk;
 use sdkgen_adapter_openapi;
-use sdkgen_core::{
-    GenerateSdk, HttpMethod, Primitive, Route, SdkResource, SdkVersion, Type, UrlParameter,
-};
-
-fn convert_http_method(method: apidoc::HttpMethod) -> HttpMethod {
-    match method {
-        apidoc::HttpMethod::Get => HttpMethod::Get,
-        apidoc::HttpMethod::Post => HttpMethod::Post,
-        apidoc::HttpMethod::Put => HttpMethod::Put,
-        apidoc::HttpMethod::Delete => HttpMethod::Delete,
-    }
-}
-
-fn convert_primitive(ty: String) -> Result<Primitive, String> {
-    match ty.as_str() {
-        "String" | "GUID" => Ok(Primitive::String),
-        "Boolean" => Ok(Primitive::Boolean),
-        "Integer" => Ok(Primitive::Integer),
-        "Float" => Ok(Primitive::Float),
-        "Number" | "Double" => Ok(Primitive::Double),
-        unknown => Err(format!("Unknown primitive: {}", unknown)),
-    }
-}
-
-fn convert_route(route: apidoc::Route) -> Route {
-    let url_parameters = route
-        .parameter
-        .map(|section| section.fields)
-        .and_then(|fields| fields.get("Parameter").cloned())
-        .map(|parameters| {
-            parameters
-                .into_iter()
-                .map(|parameter| UrlParameter {
-                    name: parameter.field,
-                    ty: convert_primitive(parameter.ty)
-                        .expect("Failed to convert to primitive type"),
-                })
-                .collect::<Vec<_>>()
-        })
-        .unwrap_or_default();
-
-    Route {
-        name: route.name,
-        method: convert_http_method(route.method),
-        url: route.url,
-        group: route.group,
-        version: route.version,
-        url_parameters,
-        payload_type: None,
-        return_type: None,
-    }
-}
+use sdkgen_core::{GenerateSdk, Route, SdkResource, SdkVersion};
 
 fn versions_from_routes(routes: Vec<Route>) -> Vec<SdkVersion> {
     let mut versions = HashMap::new();
